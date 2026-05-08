c4cda5eb04c946bce62ddb71ce271eaacb4be372
travisdodo <dodotravis100@gmail.com>
2026-05-08 16:03:57 +0900

    Add files via upload

---
+++ face/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw(){
+  background(220);
+  fill(240, 160, 130);
+  ellipse(250,250, 250,300);
+  
+  fill( 220, 220, 250)
+  ellipse(200, 200, 50, 40);
+  ellipse(300, 200, 50, 40);
+  
+  fill(0, 0, 0)
+  circle(200, 200, 25);
+  circle(300, 200, 25);
+}
